34b44c20853083631654acacbea11cf7dc3d9b27
jaswanthre <jassujaswanth405@gmail.com>
2026-07-21 02:01:45 +0530

    Remove Recent Teams UI and Visitor back button

--- client/src/routes/visitor.tsx
+++ client/src/routes/visitor.tsx
@@ -84,9 +84,7 @@ function VisitorView() {
               >
                 Logout
               </button>
-            ) : (
-              <Link to="/" className="rounded-xl border border-white/10 bg-white/5 px-4 py-2 text-sm">Back</Link>
-            )}
+            ) : null}
           </div>
         </div>
 
